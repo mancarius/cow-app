633c31f4d5e3331928a993d7664167e7d5c306ff
Mattia <75211269+mancarius@users.noreply.github.com>
2022-04-01 23:32:23 +0200

    configurated firebase auth+db

---
+++ src/configs/firebase.ts
@@ -0,0 +1,56 @@
+// Import the functions you need from the SDKs you need
+import {
+  initializeApp
+} from "firebase/app";
+import {
+  getAnalytics,
+  logEvent
+} from "firebase/analytics";
+import {
+  GoogleAuthProvider,
+  FacebookAuthProvider,
+  getAuth,
+  signInWithPopup,
+  signOut,
+} from "firebase/auth";
+import {
+  getFirestore,
+} from "firebase/firestore";
+
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_APP_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_APP_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_APP_FIREBASE_MEASUREMENT_ID,
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+const analytics = getAnalytics(app);
+logEvent(analytics, 'notification_received');
+
+
+const googleProvider = new GoogleAuthProvider();
+const facebookProvider = new FacebookAuthProvider();
+const signInWithGoogle = () => signInWithPopup(auth, googleProvider);
+const signInWithFacebook = () => signInWithPopup(auth, facebookProvider);
+
+
+const logout = () => {
+  signOut(auth);
+};
+
+
+export {
+  auth,
+  db,
+  signInWithGoogle,
+  signInWithFacebook,
+  logout,
+};
