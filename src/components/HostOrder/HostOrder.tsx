ef08575c63bf81e83c06e6f6e2d29ffa8f953465
rey-hossa <78262774+rey-hossa@users.noreply.github.com>
2022-04-04 13:42:59 +0200

    Add Host Page

--- src/components/HostOrder/HostOrder.tsx
+++ src/components/HostOrder/HostOrder.tsx
@@ -9,20 +9,24 @@ const HostOrder: React.FC = () => {
     
 
       <Box className="HostOrder">
-          <Box className="tavolo_condiviso">
-              <img src="https://source.unsplash.com/random" alt="unsplash image" />
-              <Stack>
-                  <Typography variant="h5">Tavolo Condiviso</Typography>
-                  <span><CheckIcon/> Multpresa </span>
-                  <span><CheckIcon/> Caricabatterie wireless </span>
-                  <span><CheckIcon/> Porta ethernet </span>
-              </Stack>
-
-              <Stack>
-                  <span><p>Prezzo</p> <p>$10</p></span>
-                  <span><p>Disponibilità</p> <p>2 Posti</p></span>
-                  <span><p>Quantità</p> <p>0</p></span>
-              </Stack>
+          <Box className="HostOrder_container">
+                <Box className="HostOrder_desc_box">
+                    <img src="https://source.unsplash.com/random" alt="unsplash image" />
+                    <Stack className="HostOrder_desc">
+                        <Typography variant="h5">Tavolo Condiviso</Typography>
+                        <span><CheckIcon/> <p>Multpresa</p> </span>
+                        <span><CheckIcon/> <p>Caricabatterie wireless</p> </span>
+                        <span><CheckIcon/> <p>Porta ethernet</p> </span>
+                    </Stack>
+                </Box>
+              
+                <Box className="HostOrder_info_box">
+                    <Stack className="HostOrder_info">
+                        <div><p>Prezzo</p> <span> <p>€10</p> </span></div>
+                        <div><p>Disponibilità</p> <span > <p>2/10</p> </span> </div>
+                        <div><p>Quantità</p> <span> <p>0</p> </span></div>
+                    </Stack>
+                </Box>
 
           </Box>
       </Box>
