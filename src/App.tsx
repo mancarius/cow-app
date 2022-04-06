9f002347c66117353385d4077041c92c1210a290
Mattia <75211269+mancarius@users.noreply.github.com>
2022-04-06 03:16:40 +0200

    refactoring

--- src/App.tsx
+++ src/App.tsx
@@ -7,18 +7,22 @@ import SearchResults from './views/SearchResults/SearchResults'
 import HostDetails from './views/HostDetails/HostDetails'
 
 import { Routes, Route } from 'react-router-dom'
+import Footer from "./components/Footer/Footer";
+import NavBar from "./components/NavBar/NavBar";
 
 function App() {
   return (
     <>
+      <NavBar />
       <Suspense fallback={null}>
         <Routes>
-          <Route path='*' element={<NotFound />} />
           <Route path='/' element={<Home/>} />
           <Route path='/results' element={<SearchResults/>} />
           <Route path='host-details/:id' element={<HostDetails/>} />
+          <Route path='*' element={<NotFound />} />
         </Routes>
       </Suspense>
+      <Footer></Footer>
     </>
   );
 }
--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -1,7 +1,5 @@
 import * as React from "react";
-import NavBar from "../../components/NavBar/NavBar";
 import Hero from "../../components/Hero/Hero";
-import Footer from "../../components/Footer/Footer";
 import Feature from "../../components/Feature/Feature";
 import Carousel from "../../components/Carousel/Carousel";
 import LoginDialog from "../../components/LoginDialog/LoginDialog";
@@ -9,12 +7,10 @@ import LoginDialog from "../../components/LoginDialog/LoginDialog";
 function Home() {
   return (
     <React.Fragment>
-      <NavBar />
       <LoginDialog />
       <Hero />
       <Feature />
       <Carousel />
-      <Footer />
     </React.Fragment>
   );
 }
