0b977651f16afddc45ea63e5bfe05e1583dca1a5
rey-hossa <78262774+rey-hossa@users.noreply.github.com>
2022-04-22 22:49:57 +0200

    Update HostCarousel.tsx

--- src/components/HostCarousel/HostCarousel.tsx
+++ src/components/HostCarousel/HostCarousel.tsx
@@ -2,6 +2,11 @@ import * as React from "react";
 import "./HostCarousel.css";
 import { Box, Button } from "@mui/material";
 import { Host } from "../../@types/Host";
+import image1 from "../../assets/coworking1.jpg";
+import image2 from "../../assets/coworking2.jpg";
+import image3 from "../../assets/coworking3.jpg";
+import image4 from "../../assets/coworking4.jpg";
+import image5 from "../../assets/coworking5.jpg";
 
 const HostCarousel: React.FC<{images: Host.Info["images"]}> = (props) => {
   const { images } = props;
@@ -9,7 +14,7 @@ const HostCarousel: React.FC<{images: Host.Info["images"]}> = (props) => {
 
   return (
     <Box className="HostCarousel">
-      <img className="fist_image" src={[...images].shift() ?? ''} alt="unsplash image" />
+      {/*<img className="fist_image" src={[...images].shift() ?? ''} alt="unsplash image" />
       <Box className="other_image">
         {[...images].slice(0,3)?.map((img, index) => (
           <img className={`image${index + 1}`} src={img} alt="unsplash image" />
@@ -17,7 +22,20 @@ const HostCarousel: React.FC<{images: Host.Info["images"]}> = (props) => {
         <div className="last_image">
           <Button className="last_image_button">altre foto...</Button>
         </div>
+      </Box>*/}
+
+      <img className="fist_image" src={image1} alt="unsplash image" />
+      <Box className="other_image">
+        <img className="" src={image2} alt="unsplash image" />
+        <img className="" src={image3} alt="unsplash image" />
+        <img className="image3" src={image5} alt="unsplash image" />
+        
+        <div className="last_image">
+          <Button className="last_image_button">altre foto...</Button>
+          <img className="" src={image4} alt="unsplash image" />
+        </div>
       </Box>
+
     </Box>
   );
 };
