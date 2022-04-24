bf695d71e1e6dff085776fcf2fcb17401a81ca11
Mattia <75211269+mancarius@users.noreply.github.com>
2022-04-24 16:59:11 +0200

    update booking slice

--- src/store/features/bookings/slice.ts
+++ src/store/features/bookings/slice.ts
@@ -1,16 +1,37 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
 import { createSlice } from '@reduxjs/toolkit'
 import { Booking } from '../../../@types/Booking';
+import CustomerService from '../../../service/customer.service';
 
-const initialState: {bookings: Booking.Info[]} = {
-  bookings: []
+export const addBook = createAsyncThunk("booking/add", CustomerService.addBooking);
+
+interface initialState {
+  bookings: Booking.Info['id'][];
+  status: "idle" | "loading" | "failed";
 }
 
-const bookingSlice = createSlice({
-  name: 'booking',
-  initialState,
-  reducers: {}
-});
+const initialState: initialState = {
+  bookings: [],
+  status: "idle",
+};
 
-export const {} = bookingSlice.actions
+const bookingSlice = createSlice({
+  name: "booking",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(addBook.pending, (state) => {
+        state.status = "loading";
+      })
+      .addCase(addBook.fulfilled, (state, action) => {
+        state.status = "idle";
+        state.bookings.push(action.payload);
+      })
+      .addCase(addBook.rejected, (state) => {
+        state.status = "failed";
+      });
+  },
+});
 
 export default bookingSlice.reducer
